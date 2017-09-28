98d6ca9304ded23438965b5fd4180adbb839caaa
suoknuua <31692961+suoknuua@users.noreply.github.com>
2017-09-28 15:11:49 +0300

    Update HelloWorldTest.php

--- robot/HelloWorldTest.php
+++ robot/HelloWorldTest.php
@@ -47,4 +47,9 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
         $testPHP= new testPHP();
         $this->assertEquals('test PHP', $testPHP->test());
     }
+     public function testinkii()
+    {
+        $database= new database();
+        $this->assertEquals('data base', $database->addUser($username, $first_name, $last_name, $password));
+    }
 }
