43f9a5b47f0e269c4e9c3f923c61da4a88ca9f7f
Boholm <nboholm@gmail.com>
2017-10-03 15:37:40 +0300

    Update register.php

--- src/register.php
+++ src/register.php
@@ -1,12 +1,9 @@
 <?php
+include_once 'config/database.php';
 header("Access-Control-Allow-Origin: *");
-/*include_once 'config/database.php';
-
-
 $db = new Database();
 //Create databses if not exist
 $db->createDB();
-
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
         
     $postBody = file_get_contents("php://input");
@@ -15,9 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $username = $postBody->username;
     $first_name = $postBody->first_name;
     $last_name = $postBody->last_name;
-
     $password = hash('sha256',$postBody->password);
-
     if ($username != '' && $first_name != '' && $last_name != '' && $password != '') {
         
         //Check if username already exists
@@ -28,20 +23,16 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         } else {
             //Add user          
             $db->addUser($username, $first_name, $last_name, $password);
-
             //Get userID
             $userID = $db->getUserID($username);
-
             //Add token
             $token = $db->addToken($userID);
             
             $response->status = "Registered";
             $response->token = $token;
-
             echo json_encode($response);
             http_response_code(200);           
-        }             
-
+        }            
     } else {
         $response->status = "INVALID_CREDENTIALS";
         echo json_encode($response);
@@ -53,10 +44,4 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     echo json_encode($response);
     http_response_code(404);
 }
-*/
-
-$result->status = "Registered";
-
-echo json_encode($result);
-
 ?>
